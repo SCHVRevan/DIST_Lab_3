fde0c4ea713b51de4406ab4ddb16feb93ef102d2
Egor <120785170+SCHVRevan@users.noreply.github.com>
2025-05-28 12:23:50 +0700

    Update DLLLoader.cpp
    include tchar.h

--- DLL/DLLLoader.cpp
+++ DLL/DLLLoader.cpp
@@ -1,10 +1,10 @@
-
 #include <Windows.h>
 #include <winerror.h>
+#include <tchar.h>
 
 int main()
 {
     // You also specify the complete path.
-    LoadLibrary(L"VirusDLL.dll");
+    LoadLibrary(_T("VirusDLL.dll"));
     return 0;
 }
